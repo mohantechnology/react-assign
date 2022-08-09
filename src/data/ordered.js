2ebb4e153e63af9f124e3702f333e759cf8f85d1
Mohan <mohansahualbert@gmail.com>
2022-08-09 13:50:08 +0530

    added correct img and descrption of product

---
+++ src/data/ordered.js
@@ -0,0 +1,25 @@
+import  product from "product";  
+ 
+export default [
+  {
+    productDetail: product.slice(0,2) ,
+    clientDetail: {
+      firstName: "Mary",
+      lastName: "Doe",
+      email: "john@gm.com", 
+      _id: "2",
+
+    },
+    createdAt: "23/01/1990",
+    updatedAt: "24/01/1990",
+
+    isDiscounted: true,
+    totalPrice: 13220,
+    discountPrice: 1300,
+  },
+]
+
+// console.log( ordered)
+
+
+
--- src/data/product.js
+++ src/data/product.js
@@ -1,9 +1,9 @@
-export default [
+ const data =  [
 
     {
-      title: "This is card this is tile ",
-      desc: "lps someone who is hungry, but at a cost of being hungry yoursel",
-      img: "https://gw.alipayobjects.com/zos/rmsportal/JiqGstEfoWAOHiTxclqi.png",
+      title: "Analog Blue Dial Men's Watch-FK00010E",
+      desc: "The perfect everyday accessory, this FCUK watch is both practical and stylish. Strap Colour: Blue. Dial Colour: Blue. Product Material: Silicone.",
+      img: "watch.jpg",
       _id: "0",
       price: "1000",
       discountPercentage: "17",
@@ -11,20 +11,20 @@ export default [
   
       about: [
         {
-          title: "Processor",
-          desc: " MediaTek Helio G25 Octa-core; Up to 2.0GHz clock speed Camera: 13 MP Rear camera with AI portrait| 5 MP front camera",
+          title: "Dial Color",
+          desc: "Case Shape: Round, Dial Glass Material: Minera",
         },
         {
-          title: "Display",
-          desc: " 16.58 centimeters (6.53-inch) HD+ display with 720x1600 pixels and 20:9 aspect ratio",
+          title: "Watch Movement Type",
+          desc: "Quartz, Watch Display Type: Analog",
         },
         {
-          title: "Battery",
-          desc: " 5000 mAH large battery with 10W wired charger in-box",
+          title: "Water Resistance Depth",
+          desc: "30 meters",
         },
         {
-          title: "Memory",
-          desc: " Storage & SIM: 3GB RAM | 32GB storage | Dual SIM (nano+nano) + Dedicated SD card slot",
+          title: "Case Material",
+          desc: " Stainless Steel, Dial Diameter: 43.5 millimeters",
         },
         {
           title: "form_factor",
@@ -56,33 +56,33 @@ export default [
       ],
     },
     {
-      title: "Altruism refers to behavior",
-      desc: "Altruism refers to behavior that benefits another individual at a cost to oneself. For example, giving your lunch away is altruistic because it helps someone who is hungry, but at a cost of being hungry yoursel",
-      img: "http://localhost:3000/logo.png",
+      title: "Relaxed & Fashionable Men's Casual Shoes",
+      desc: "Mesh Sole Material: Rubber Fastening & Back Detail: Lace-Up Multipack: 1 Sizes: IND-6, IND-7, IND-8",
+      img: "shoes.jpg",
       _id: "1",
       price: "1000",
       discountPercentage: "32",
       rating: 3,
       about: [
         {
-          title: "Processor",
-          desc: " MediaTek Helio G25 Octa-core; Up to 2.0GHz clock speed Camera: 13 MP Rear camera with AI portrait| 5 MP front camera",
+          title: "Sole",
+          desc: " Rubber",
         },
         {
-          title: "Display",
-          desc: " 16.58 centimeters (6.53-inch) HD+ display with 720x1600 pixels and 20:9 aspect ratio",
+          title: "Closure",
+          desc: " Lace-Up",
         },
         {
-          title: "Battery",
-          desc: " 5000 mAH large battery with 10W wired charger in-box",
+          title: "Shoe Width",
+          desc: "Medium",
         },
         {
-          title: "Memory",
-          desc: " Storage & SIM: 3GB RAM | 32GB storage | Dual SIM (nano+nano) + Dedicated SD card slot",
+          title: "Material",
+          desc: "Mesh",
         },
         {
-          title: "form_factor",
-          desc: "Bar,operating_system:MIUI 12",
+          title: "Closure",
+          desc: "Lace-Up",
         },
       ],
   
@@ -111,11 +111,11 @@ export default [
     },
   
     {
-      title: "This is card this is tile ",
-      desc: "lps someone who is hungry, but at a cost of being hungry yoursel",
-      img: "https://gw.alipayobjects.com/zos/rmsportal/JiqGstEfoWAOHiTxclqi.png",
+      title: "boAt Bassheads 100 in Ear Wired Earphones with Mic(Black)",
+      desc: "boAt BassHeads 100 Stereo In-Ear Earphones are designed to give you an experience that no other product can match. As the headphones feature 10mm drivers",
+      img: "ear-phone.jpg",
       _id: "2",
-      price: "1000",
+      price: "329.0",
       discountPercentage: "17",
       rating: 3,
   
@@ -167,11 +167,11 @@ export default [
     },
    
     {
-      title: "This is card this is tile ",
-      desc: "lps someone who is hungry, but at a cost of being hungry yoursel",
-      img: "https://gw.alipayobjects.com/zos/rmsportal/JiqGstEfoWAOHiTxclqi.png",
+      title: "boAt Wave Lite Smartwatch",
+      desc: "boAt Wave Lite Smartwatch with 1.69' HD Display, Sleek Metal Body, HR & SpO2 Level Monitor, 140+ Watch Faces, Activity Tracker, Multiple Sports Modes, IP68 & 7 Days Battery Life(Active Black)",
+      img: "watch.jpg",
       _id: "3",
-      price: "1000",
+      price: "159",
       discountPercentage: "32",
       rating: 3,
   
@@ -223,11 +223,11 @@ export default [
     },
   
     {
-      title: "This is card this is tile ",
-      desc: "lps someone who is hungry, but at a cost of being hungry yoursel",
-      img: "https://joeschmoe.io/api/v1/random",
+      title: "Fire TV Stick ",
+      desc: "Fire TV Stick (3rd Gen, 2021) with Alexa Voice Remote (includes TV and app controls) | HD streaming device",
+      img: "laptop.jpg",
       _id: "4",
-      price: "1000",
+      price: "599.0",
       discountPercentage: "5",
   
       rating: 3,
@@ -280,14 +280,13 @@ export default [
     },
   
     {
-      title: "Altruism refers to behavior",
-      desc: "Altruism refers to behavior that benefits another individual at a cost to oneself. For example, giving your lunch away is altruistic because it helps someone who is hungry, but at a cost of being hungry yoursel",
-      img: "http://localhost:3000/logo.png",
+      title: "Echo Show 10- 10.1",
+      desc: "HD smart display with motion, premium sound and Alexa (Black)",
+      img: "thinkpad.jpg",
       _id: "5",
-      price: "1000",
+      price: "999",
       discountPercentage: "32",
-      rating: 3,
-  
+      rating: 3, 
       about: [
         {
           title: "Processor",
@@ -336,9 +335,9 @@ export default [
     },
   
     {
-      title: "This is card this is tile ",
-      desc: "lps someone who is hungry, but at a cost of being hungry yoursel",
-      img: "https://gw.alipayobjects.com/zos/rmsportal/JiqGstEfoWAOHiTxclqi.png",
+      title: "Pigeon Amaze Plus Electric Kettle",
+      desc: "Pigeon Amaze Plus Electric Kettle (14289) with Stainless Steel Body, 1.5 litre, used for boiling Water, making tea and coffee, instant noodles, soup etc. 1500 Watt (Silver)",
+      img: "kettle.webp",
       _id: "6",
       price: "1000",
       discountPercentage: "17",
@@ -346,24 +345,24 @@ export default [
   
       about: [
         {
-          title: "Processor",
-          desc: " MediaTek Helio G25 Octa-core; Up to 2.0GHz clock speed Camera: 13 MP Rear camera with AI portrait| 5 MP front camera",
+          title: "CLASSIC DESIGN",
+          desc: "The classical mirror polish of the appearance makes your electric kettle unique and aesthetic, which can match any type of kitchen design and 360° swivel base is connected with standard power cord for safe usage and convenient storage",
         },
         {
-          title: "Display",
-          desc: " 16.58 centimeters (6.53-inch) HD+ display with 720x1600 pixels and 20:9 aspect ratio",
+          title: "CORDLESS POURING",
+          desc: "The electric kettle can be easily lifts from its swivel base for easy filling at the sink and graceful serving without the hassle of a power cord; it can also be returned to its power source from any direction, great for right and left-handed users.",
         },
         {
-          title: "Battery",
-          desc: " 5000 mAH large battery with 10W wired charger in-box",
+          title: "CONVENIENT",
+          desc: "he cordless & BPA-Free electric electric kettle makes a striking presence on any kitchen counter or buffet in preparing and serving and it is easy for storage",
         },
         {
           title: "Memory",
           desc: " Storage & SIM: 3GB RAM | 32GB storage | Dual SIM (nano+nano) + Dedicated SD card slot",
         },
         {
-          title: "form_factor",
-          desc: "Bar,operating_system:MIUI 12",
+          title: "FAST BOIL",
+          desc: "This 1500W electric kettle has concealed heating elements and can boil up to 1.5 litres of water in 5 to 7 minutes, quicker and safer. Start your day with a cup of instant lemon tea, green tea, hot water",
         },
       ],
   
@@ -392,11 +391,11 @@ export default [
     },
   
     {
-      title: "This is card this is tile ",
-      desc: "lps someone who is hungry, but at a cost of being hungry yoursel",
-      img: "https://gw.alipayobjects.com/zos/rmsportal/JiqGstEfoWAOHiTxclqi.png",
+      title: "Lenovo Latitude Laptop X260 Intel Core i3",
+      desc: "(Renewed) Lenovo Latitude Laptop X260 Intel Core i3 - 6100u Processor 6th Gen, 8 GB Ram & 128 GB SSD, 12.5 Inches (Ultra Slim & Feather Light 1.4KG) Notebook Computer",
       _id: "7",
-      price: "1000",
+      img: "laptop.jpg" ,
+      price: "12000",
       discountPercentage: "32",
       rating: 3,
   
@@ -448,11 +447,11 @@ export default [
     },
   
     {
-      title: "This is card this is tile ",
-      desc: "lps someone who is hungry, but at a cost of being hungry yoursel",
-      img: "https://joeschmoe.io/api/v1/random",
+      title: "Lenovo ThinkCentre M71E Desktop",
+      desc: "(Renewed) Lenovo ThinkCentre M71E Desktop (Core I3 3.3 Ghz, 8 GB RAM/ 500 GB HDD/ Win 10 Pro, MS Office/ Intel H61 Express/ USB, Ethernet,VGA,Display Port,Audio, Black",
+      img: "thinkpad.jpg",
       _id: "8",
-      price: "1000",
+      price: "10200",
       discountPercentage: "5",
   
       rating: 3,
@@ -504,3 +503,11 @@ export default [
       ],
     },
   ]
+ 
+  data.map((item )=>{ 
+
+    item.img = window.location.origin + "/img/"+ item.img ; 
+  })
+  
+  
+  export default data ; 
